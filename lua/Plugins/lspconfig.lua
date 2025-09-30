11c8b7ed07e5d3ed3db92d0a15bcdbc438ee3c93
Abdul Alim Mahir <mahir.mj143@gmail.com>
2025-09-30 13:58:24 +0600

    merged with mason+tool+mason-lspconfig

--- lua/Plugins/lspconfig.lua
+++ lua/Plugins/lspconfig.lua
@@ -20,43 +20,89 @@ return {
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
-		-- vim.api.nvim_create_autocmd("LspAttach", {
-		-- 	group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-		-- 	callback = function(event)
-		-- 		local map = function(keys, func, desc, mode)
-		-- 			mode = mode or "n"
-		-- 			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-		-- 		end
-		-- 		map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
-		-- 		map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-		-- 		map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-		-- 		map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-		-- 		map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-		-- 		map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-		-- 		map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
-		-- 		map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
-		-- 		map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
-		-- 	end,
-		-- })
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+			callback = function(event)
+				local map = function(keys, func, desc, mode)
+					mode = mode or "n"
+					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				end
+				map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+				map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+				map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+				map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+				map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+				map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+				map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
+				map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
+				map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+			end,
+		})
+
+		local filetype = vim.bo.filetype
+		local language_server_map = {
+			lua = { "lua_ls", "stylua", "selene" },
+			python = { "pyright", "black", "pylint" },
+			html = { "html", "htmlhint" },
+			css = { "cssls", "stylelint", "tailwindcss" },
+			javascript = { "ts_ls", "prettierd", "biome" },
+			typescript = { "ts_ls", "prettierd", "biome" },
+			markdown = { "marksman", "markdownlint" },
+			cpp = { "clangd", "cpplint" },
+			bash = { "bashls" },
+			json = { "jsonlint" },
+			powershell = { "powershell_es" },
+			ps1 = { "powershell_es" },
+			psd1 = { "powershell_es" },
+			psm1 = { "powershell_es" },
+			toml = { "taplo" },
+		}
+		local ensure_installed = language_server_map[filetype] or {}
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		local lspconfig = require("lspconfig")
+		local servers = {
+			lua_ls = {
+				filetypes = { "lua" },
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+						runtime = { version = "LuaJIT" },
+						completion = { callSnippet = "Replace" },
+						workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+					},
+				},
+			},
+			html = {
+				filetypes = { "html", "templ" }, -- plural
+				root_dir = lspconfig.util.root_pattern("package.json", ".git"),
+				init_options = {
+					provideFormatter = true, -- fixed typo
+					embeddedLanguages = { css = true, javascript = true },
+					configurationSection = { "html", "css", "javascript" },
+				},
+			},
+
+			powershell_es = {
+				filetypes = { "psm1", "ps1", "psd1" },
+				settings = {
+					powershell = {
+						codeFormatting = { preset = "OTBS" },
+					},
+				},
+				init_options = {
+					enableProfileLoading = false,
+				},
+				-- bundle_path = "~/AppData/Local/nvim-data/mason/packages/powershell-editor-services",
+				root_dir = lspconfig.util.root_pattern("PSScriptAnalyzerSettings.psd1", ".git"),
+				single_file_support = true,
+			},
+		}
 
 		vim.diagnostic.config({
-			virtual_text = {
-				spacing = 2,
-				prefix = "●",
-				source = "if_many",
-				format = function(diagnostic)
-					local diagnostic_message = {
-						[vim.diagnostic.severity.ERROR] = diagnostic.message,
-						[vim.diagnostic.severity.WARN] = diagnostic.message,
-						[vim.diagnostic.severity.INFO] = diagnostic.message,
-						[vim.diagnostic.severity.HINT] = diagnostic.message,
-					}
-					return diagnostic_message[diagnostic.severity]
-				end,
-			},
+			virtual_text = false,
 			virtual_lines = false,
 			underline = { severity = vim.diagnostic.severity.ERROR },
-			update_in_insert = false,
+			update_in_insert = true,
 			severity_sort = true,
 			signs = {
 				text = {
@@ -67,7 +113,7 @@ return {
 				},
 			},
 			inlay_hints = {
-				enabled = true,
+				enabled = filetype == "lua" or filetype == "typescript",
 				exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
 			},
 			codelens = {
@@ -90,72 +136,6 @@ return {
 				border = "rounded",
 			},
 		})
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
-		local lspconfig = require("lspconfig")
-		local servers = {
-			lua_ls = {
-				settings = {
-					Lua = {
-						diagnostics = { globals = { "vim" } },
-						runtime = { version = "LuaJIT" },
-						completion = { callSnippet = "Replace" },
-						workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-					},
-				},
-			},
-			html = {
-				filetypes = { "html", "templ" }, -- plural
-				root_dir = lspconfig.util.root_pattern("package.json", ".git"),
-				init_options = {
-					provideFormatter = true, -- fixed typo
-					embeddedLanguages = { css = true, javascript = true },
-					configurationSection = { "html", "css", "javascript" },
-				},
-			},
-
-			powershell_es = {
-				settings = {
-					powershell = {
-						codeFormatting = { preset = "OTBS" },
-					},
-				},
-				root_dir = lspconfig.util.root_pattern("PSScriptAnalyzerSettings.psd1", ".git"),
-				single_file_support = true,
-			},
-		}
-		local ensure_installed = vim.tbl_keys(servers or {})
-		vim.list_extend(ensure_installed, {
-			"bashls", --Bash
-			"biome", --JSon, JavaScript, TypeScript
-			"clangd", --C/C++
-			"cssls", --CSS,SCSS,LESS
-			"html", --HTML
-			"lua_ls", --Lua (formerly "sumneko_lua")
-			"marksman", --Markdown LSP
-			"powershell_es", --PowerShell Script
-			"pyright", --Python LS
-			"tailwindcss", --Tailwind CSS
-			"taplo", --TOML
-			"ts_ls", --TypeScript
-			---|>(3)DAP
-			"codelldb", --C, C++, Rust, Zig
-			"cpptools", --C, C++, Rust
-			"js-debug-adapter", --JavaScript, TypeScript
-			"local-lua-debugger-vscode", --Lua
-			---|>(4)Linter
-			"cpplint", --C,C++
-			"gitlint",
-			"htmlhint", --HTML
-			"jsonlint", --JSON
-			"markdownlint", --Markdown
-			"pylint", --Python
-			"selene", --Lua, Luau
-			"stylelint", --CSS, Sass, SCSS, LESS
-			---|>(5)Formatter
-			"black", --Python
-			"prettierd", --prettier in CLI Deamon
-			"stylua", --Lua
-		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 		require("mason-lspconfig").setup({
 			ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
@@ -164,7 +144,11 @@ return {
 				function(server_name)
 					local server = servers[server_name] or {}
 					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-					require("lspconfig")[server_name].setup(server)
+					require("lspconfig")[server_name].setup(vim.tbl_deep_extend("force", {
+						flags = {
+							debounce_text_changes = 150,
+						},
+					}, server))
 				end,
 			},
 		})
